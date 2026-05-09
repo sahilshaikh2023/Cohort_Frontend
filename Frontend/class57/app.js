bdb5cc5c2b3e3e5d7dfc2e6267985446ab2acd59
sahilshaikh2023 <sahilshaikh6922@gmail.com>
2026-05-09 11:20:23 +0530

    update

---
+++ Frontend/class57/app.js
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////
+//     
+//     Function name :    bootle        
+//     Input :                       No
+//     Output :                    String
+//     Description :            Perform Demostration of class and Object
+//     Date :                        09/05/2026
+//     Author                       Sahil Latif Shaikh
+//
+//
+////////////////////////////////////////////////////////////////
+class bottle
+{
+       constructor()                                                  // constructor
+       {
+              this.color = "blue";
+              this.price = 25 ; 
+              this.metal = "steel";
+
+       }
+
+       panipeoo(){};                                                 // Method 1 treated as a prototype
+       bottleuthao (){};                                            // Method2 treated as a prototype
+       
+}
+
+
+////////////////////////////////////////////////////////////////
+//
+//     Creating the Object 
+//
+////////////////////////////////////////////////////////////////
+
+
+ let bottle1 = new bottle();                                   //  Create object bottle1
+ let bottle2 = new bottle();                                  //  Create object bottle2
